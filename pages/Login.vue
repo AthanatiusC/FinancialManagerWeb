a30c2399a1d0008fd6f1f86757debc0da28898a5
AthanatiusC <lexi_anugrah@Hotmail.com>
2021-04-23 02:33:44 +0700

    Deploy Fix Firebase

--- pages/Login.vue
+++ pages/Login.vue
@@ -4,11 +4,22 @@
             <v-row align="center" justify="center">
                 <v-col md="3">
                     <v-card color="#f4f7f5" shaped elevation="10">
-                    <div align="center" style="height:100px">
-                        <img style="margin:20px;max-height:200px;display:block;position:absolute;top:-30%;left:23.5%" :src="require('@/assets/icons/Fingerprint Padlock.png')" alt="">
+                    <v-flex>
+                        <v-img class="mx-auto" contain max-width="200px" :src="require('@/assets/icons/Fingerprint Padlock.png')" alt="">
+                        </v-img>
                         <!-- <h2 class="display-3">Login</h2> -->
-                    </div> 
+                    </v-flex> 
                     <v-card-text>
+                        <v-row v-show="isLogin" justify="center" >
+                            <div class="semipolar-spinner text-center" :style="spinnerStyle">
+                            <div class="ring"></div>
+                            <div class="ring"></div>
+                            <div class="ring"></div>
+                            <div class="ring"></div>
+                            <div class="ring"></div>
+                            </div>
+                        </v-row>
+                        <br>
                         <h2 class="text-center">Enter your user creditential!</h2>
                         <br>
                         <v-form ref="form" v-model="valid" lazy-validation>
@@ -90,7 +101,8 @@ export default{
             message:'',
             transition:''
         },
-        valid:false
+        valid:false,
+        isLogin:false
     }),
     head(){
       return{
@@ -107,6 +119,7 @@ export default{
             }, 3000);
         },
         async login(){
+            this.isLogin = true
             if(this.$refs.form.validate()){
                 await this.$axios.$post("/api/v1/user/auth",this.user).then((res)=>{
                     if(res.data.id!= null){
@@ -135,7 +148,80 @@ export default{
                     }
                 })
             }
+            this.isLogin = false
         }
     }
 }
 </script>
+
+<style scoped>
+.semipolar-spinner, .semipolar-spinner * {
+      box-sizing: border-box;
+    }
+
+    .semipolar-spinner {
+      height: 65px;
+      width: 65px;
+      position: relative;
+    }
+
+    .semipolar-spinner .ring {
+      border-radius: 50%;
+      position: absolute;
+      border: calc(65px * 0.05) solid transparent;
+      border-top-color: #ff1d5e;
+      border-left-color: #ff1d5e;
+      animation: semipolar-spinner-animation 2s infinite;
+    }
+
+    .semipolar-spinner .ring:nth-child(1) {
+      height: calc(65px - 65px * 0.2 * 0);
+      width: calc(65px - 65px * 0.2 * 0);
+      top: calc(65px * 0.1 * 0);
+      left: calc(65px * 0.1 * 0);
+      animation-delay: calc(2000ms * 0.1 * 4);
+      z-index: 5;
+    }
+
+    .semipolar-spinner .ring:nth-child(2) {
+      height: calc(65px - 65px * 0.2 * 1);
+      width: calc(65px - 65px * 0.2 * 1);
+      top: calc(65px * 0.1 * 1);
+      left: calc(65px * 0.1 * 1);
+      animation-delay: calc(2000ms * 0.1 * 3);
+      z-index: 4;
+    }
+
+    .semipolar-spinner .ring:nth-child(3) {
+      height: calc(65px - 65px * 0.2 * 2);
+      width: calc(65px - 65px * 0.2 * 2);
+      top: calc(65px * 0.1 * 2);
+      left: calc(65px * 0.1 * 2);
+      animation-delay: calc(2000ms * 0.1 * 2);
+      z-index: 3;
+    }
+
+    .semipolar-spinner .ring:nth-child(4) {
+      height: calc(65px - 65px * 0.2 * 3);
+      width: calc(65px - 65px * 0.2 * 3);
+      top: calc(65px * 0.1 * 3);
+      left: calc(65px * 0.1 * 3);
+      animation-delay: calc(2000ms * 0.1 * 1);
+      z-index: 2;
+    }
+
+    .semipolar-spinner .ring:nth-child(5) {
+      height: calc(65px - 65px * 0.2 * 4);
+      width: calc(65px - 65px * 0.2 * 4);
+      top: calc(65px * 0.1 * 4);
+      left: calc(65px * 0.1 * 4);
+      animation-delay: calc(2000ms * 0.1 * 0);
+      z-index: 1;
+    }
+
+    @keyframes semipolar-spinner-animation {
+      50% {
+        transform: rotate(360deg) scale(0.7);
+      }
+    }
+</style>
